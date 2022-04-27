adaa2893e9473d4a9333e50d6dee435864f572a9
sjjeong <sjjeong94@gmail.com>
2022-04-27 18:50:17 +0900

    add mnist.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import mnist
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,14 +15,11 @@ def get_accuracy(model, x, y):
 def test_grad():
     np.random.seed(1234)
 
-    train_image = np.load('./data/train_image.npy')
-    train_label = np.load('./data/train_label.npy')
-    test_image = np.load('./data/test_image.npy')
-    test_label = np.load('./data/test_label.npy')
+    train_image, train_label, test_image, test_label = mnist.load()
 
-    x_train = train_image.reshape(-1, 784) / 255.
+    x_train = train_image / 255.
     y_train = train_label
-    x_test = test_image.reshape(-1, 784) / 255.
+    x_test = test_image / 255.
     y_test = test_label
 
     y_onehot = np.zeros((len(y_train), 10), float)
